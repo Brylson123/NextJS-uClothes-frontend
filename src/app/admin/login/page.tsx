fd6e61a76e8ec3ed0bd219b8b90f1704abe23f68
Bryła <csowscy@gmail.com>
2024-11-27 12:01:49 +0100

    Add check token for auto login

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,17 +1,37 @@
 'use client';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useRouter} from 'next/navigation';
 
 export default function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState<string | null>(null);
-    const [loading, setLoading] = useState<boolean>(false);
+    const [loading, setLoading] = useState(false);
     const [message, setMessage] = useState<string | null>(null);
     const [messageType, setMessageType] = useState<'success' | 'error' | null>(null);
     const router = useRouter();
 
-    const handleSubmit = async (e: React.FormEvent ) => {
+    useEffect(() => {
+        const checkToken = async () => {
+            try {
+                const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/validateToken`, {
+                    method: 'GET',
+                    credentials: 'include',
+                });
+                const data = await res.json();
+
+                if (res.ok && data.success) {
+                    router.push('/admin/dashboard');
+                }
+            } catch (error) {
+                console.error('Error validating token:', error);
+            }
+        };
+
+        checkToken();
+    }, [router]);
+
+    const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
         setError(null);
         setMessage(null);
@@ -29,14 +49,14 @@ export default function Login() {
                 method: 'POST',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify({username, password}),
-                credentials: 'include'
+                credentials: 'include',
             });
             const data = await res.json();
 
             setLoading(false);
 
             if (!res.ok || !data.success) {
-                setMessage('Username or password are incorrect!');
+                setMessage('Invalid username or password.');
                 setMessageType('error');
             } else {
                 setMessage('Successfully logged in!');
@@ -45,9 +65,9 @@ export default function Login() {
                     router.push('/admin/dashboard');
                 }, 1000);
             }
-        } catch (err: any) {
+        } catch (error: any) {
             setLoading(false);
-            setError(err.message);
+            setError(error.message || 'An unknown error occurred.');
             setMessageType('error');
         }
     };
@@ -59,7 +79,9 @@ export default function Login() {
                 {error && <p className="text-red-500 mb-4">{error}</p>}
                 <form onSubmit={handleSubmit} className="space-y-4">
                     <div>
-                        <label htmlFor="username" className="block mb-1">Username:</label>
+                        <label htmlFor="username" className="block mb-1">
+                            Username:
+                        </label>
                         <input
                             type="text"
                             id="username"
@@ -69,7 +91,9 @@ export default function Login() {
                         />
                     </div>
                     <div>
-                        <label htmlFor="password" className="block mb-1">Password:</label>
+                        <label htmlFor="password" className="block mb-1">
+                            Password:
+                        </label>
                         <input
                             type="password"
                             id="password"
@@ -84,23 +108,18 @@ export default function Login() {
                             className="w-full py-2 px-4 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none"
                             disabled={loading}
                         >
-                            {loading ? (
-                                <svg className="animate-spin h-5 w-5 mx-auto" xmlns="http://www.w3.org/2000/svg"
-                                     fill="none" viewBox="0 0 24 24">
-                                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
-                                            strokeWidth="4"></circle>
-                                    <path className="opacity-75" fill="currentColor"
-                                          d="M4 12a8 8 0 018-8V0C5.372 0 0 5.372 0 12h4z"></path>
-                                </svg>
-                            ) : (
-                                'Login'
-                            )}
+                            {loading ? 'Logging in...' : 'Login'}
                         </button>
                     </div>
                 </form>
                 {message && (
                     <div
-                        className={`mt-4 p-4 border rounded ${messageType === 'success' ? 'bg-green-100 border-green-400 text-green-700' : 'bg-red-100 border-red-400 text-red-700'}`}>
+                        className={`mt-4 p-4 border rounded ${
+                            messageType === 'success'
+                                ? 'bg-green-100 border-green-400 text-green-700'
+                                : 'bg-red-100 border-red-400 text-red-700'
+                        }`}
+                    >
                         {message}
                     </div>
                 )}
